e165503eaae31aeae112d6f028d97185c8f77d3a
Amber Morris <ambermorris1997@gmail.com>
2026-08-25 14:37:59 -0400

    add get request to fetch all movies

--- src/main/java/app/controllers/MovieController.java
+++ src/main/java/app/controllers/MovieController.java
@@ -9,14 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.google.genai.Client;
 import com.google.genai.types.GenerateContentResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/movies")
@@ -24,6 +22,12 @@ public class MovieController {
 
     @Autowired
     MovieRepository movieRepository;
+
+    @GetMapping("")
+    public ResponseEntity<?> getAllMovies() {
+        List<Movie> allMovies = movieRepository.findAll();
+        return new ResponseEntity<>(allMovies, HttpStatus.OK);
+    }
 
     @PostMapping(value = "/add", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> addMovie(@RequestBody MovieDTO movieData) throws HttpException, IOException {
